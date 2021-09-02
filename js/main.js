0d41d498a570520fd54219d8303dfdf551b284d6
alberto <albertozanola1996@gmail.com>
2021-09-02 16:08:31 +0200

    Milestone 2

--- js/main.js
+++ js/main.js
@@ -1,118 +1,149 @@
 const icons = [
-    {
-      name: 'cat',
-      prefix: 'fa-',
-      type: 'animal',
-      family: 'fas',
-    },
-    {
-      name: 'crow',
-      prefix: 'fa-',
-      type: 'animal',
-      family: 'fas',
-    },
-    {
-      name: 'dog',
-      prefix: 'fa-',
-      type: 'animal',
-      family: 'fas',
-    },
-    {
-      name: 'dove',
-      prefix: 'fa-',
-      type: 'animal',
-      family: 'fas',
-    },
-    {
-      name: 'dragon',
-      prefix: 'fa-',
-      type: 'animal',
-      family: 'fas',
-    },
-    {
-      name: 'horse',
-      prefix: 'fa-',
-      type: 'animal',
-      family: 'fas',
-    },
-    {
-      name: 'hippo',
-      prefix: 'fa-',
-      type: 'animal',
-      family: 'fas',
-    },
-    {
-      name: 'fish',
-      prefix: 'fa-',
-      type: 'animal',
-      family: 'fas',
-    },
-    {
-      name: 'carrot',
-      prefix: 'fa-',
-      type: 'vegetable',
-      family: 'fas',
-    },
-    {
-      name: 'apple-alt',
-      prefix: 'fa-',
-      type: 'vegetable',
-      family: 'fas',
-    },
-    {
-      name: 'lemon',
-      prefix: 'fa-',
-      type: 'vegetable',
-      family: 'fas',
-    },
-    {
-      name: 'pepper-hot',
-      prefix: 'fa-',
-      type: 'vegetable',
-      family: 'fas',
-    },
-    {
-      name: 'user-astronaut',
-      prefix: 'fa-',
-      type: 'user',
-      family: 'fas',
-    },
-    {
-      name: 'user-graduate',
-      prefix: 'fa-',
-      type: 'user',
-      family: 'fas',
-    },
-    {
-      name: 'user-ninja',
-      prefix: 'fa-',
-      type: 'user',
-      family: 'fas',
-    },
-    {
-      name: 'user-secret',
-      prefix: 'fa-',
-      type: 'user',
-      family: 'fas',
-    },
-  ];
+  {
+    name: 'cat',
+    prefix: 'fa-',
+    type: 'animal',
+    family: 'fas',
+  },
+  {
+    name: 'crow',
+    prefix: 'fa-',
+    type: 'animal',
+    family: 'fas',
+  },
+  {
+    name: 'dog',
+    prefix: 'fa-',
+    type: 'animal',
+    family: 'fas',
+  },
+  {
+    name: 'dove',
+    prefix: 'fa-',
+    type: 'animal',
+    family: 'fas',
+  },
+  {
+    name: 'dragon',
+    prefix: 'fa-',
+    type: 'animal',
+    family: 'fas',
+  },
+  {
+    name: 'horse',
+    prefix: 'fa-',
+    type: 'animal',
+    family: 'fas',
+  },
+  {
+    name: 'hippo',
+    prefix: 'fa-',
+    type: 'animal',
+    family: 'fas',
+  },
+  {
+    name: 'fish',
+    prefix: 'fa-',
+    type: 'animal',
+    family: 'fas',
+  },
+  {
+    name: 'carrot',
+    prefix: 'fa-',
+    type: 'vegetable',
+    family: 'fas',
+  },
+  {
+    name: 'apple-alt',
+    prefix: 'fa-',
+    type: 'vegetable',
+    family: 'fas',
+  },
+  {
+    name: 'lemon',
+    prefix: 'fa-',
+    type: 'vegetable',
+    family: 'fas',
+  },
+  {
+    name: 'pepper-hot',
+    prefix: 'fa-',
+    type: 'vegetable',
+    family: 'fas',
+  },
+  {
+    name: 'user-astronaut',
+    prefix: 'fa-',
+    type: 'user',
+    family: 'fas',
+  },
+  {
+    name: 'user-graduate',
+    prefix: 'fa-',
+    type: 'user',
+    family: 'fas',
+  },
+  {
+    name: 'user-ninja',
+    prefix: 'fa-',
+    type: 'user',
+    family: 'fas',
+  },
+  {
+    name: 'user-secret',
+    prefix: 'fa-',
+    type: 'user',
+    family: 'fas',
+  },
+];
+
+const colors = [
+  'blue',
+  'orange',
+  'purple'
+]
+
 
 const iconsContainer = document.getElementById('icons');
 
-print(icons, iconsContainer);
+const coloredArray = colorIcons(icons, colors);
+print(coloredArray, iconsContainer);
+
 
 function print(array, container) {
-    container.innerHTML = '';
+  container.innerHTML = '';
 
-    array.forEach(element => {
-        const {name, family, prefix, type} = element;
-        container.innerHTML += `
-        <div>
-            <i class="${family} ${prefix}${name}" style="color: blue;"></i>
-            <div class="title">${name.toUpperCase()}</div>
-        </div>
-        `;
-    });
+  array.forEach(element => {
+    const {name, family, prefix, color} = element;
+    container.innerHTML += `
+    <div>
+      <i class="${family} ${prefix}${name}" style="color: ${color};"></i>
+      <div class="title">${name.toUpperCase()}</div>
+    </div>`;
+  });
+} 
+
+function colorIcons(array, colors) {
+  const types = getTypes(array);
+  const coloredArray = array.map((element) => {
+    const indexType = types.indexOf(element.type);
+
+    element.color = colors[indexType];
+    return element;
+  });
+  return coloredArray;
+}
+
+function getTypes(array) {
+  const types = [];
+
+  array.forEach((element) => {
+    if( types.includes(element.type) == false) {
+      types.push(element.type);
+    }
+
+  });
+  return types;
 }
 
 
